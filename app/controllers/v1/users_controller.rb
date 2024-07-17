5b0b5d4e30ea139299c29c9477c6b2dac1d70026
rubicon44 <rub.b.c44@gmail.com>
2024-07-17 09:57:31 +0900

    [Add]Add users controller.

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,44 @@
+module V1
+  class UsersController < ApiController
+    def index
+      users = User.all
+      render json: users
+    end
+
+    def show
+      user = User.find(params[:id])
+      render json: user
+    end
+
+    def create
+      user = User.new(user_params)
+
+      if user.save
+        render json: user, status: :created
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      user = User.find(params[:id])
+      if user.update(user_params)
+        render json: user
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      user = User.find(params[:id])
+      user.destroy
+      render json: {}, status: :no_content
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:firebase_id, :bio, :email, :nickname, :username)
+    end
+  end
+end
