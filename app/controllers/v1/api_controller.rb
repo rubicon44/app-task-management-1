b7b3007b0e522e054b91cbf1cd8171856a73325b
rubicon44 <rub.b.c44@gmail.com>
2024-11-06 23:12:20 +0900

    [Add]jwt検証機能作成

--- app/controllers/v1/api_controller.rb
+++ app/controllers/v1/api_controller.rb
@@ -1,4 +1,36 @@
 module V1
   class ApiController < ApplicationController
+    before_action :check_authenticate!
+
+    private
+
+    def check_authenticate!
+      token = request.headers['Authorization']
+      return render_unauthorized('Authorization token is missing') if token.blank?
+
+      begin
+        decoded = JsonWebToken.decode(token)
+        user = User.find_by(firebase_id: decoded[:firebase_id])
+
+        if user.nil?
+          render_unauthorized('User not found')
+          return
+        end
+
+        requested_user_id = params[:id].to_i
+        if user.id != requested_user_id
+          render_unauthorized('Unauthorized access to user information')
+          return
+        end
+      rescue ActiveRecord::RecordNotFound => e
+        render_unauthorized(e.message)
+      rescue JWT::DecodeError => e
+        render_unauthorized(e.message)
+      end
+    end
+
+    def render_unauthorized(errors)
+      render json: { errors: errors }, status: :unauthorized
+    end
   end
 end
---
+++ app/lib/firebase.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Firebase
+  def self.get_user_by_token(token)
+    RestClient.post(
+      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo?key=#{ENV['FIREBASE_API_KEY']}",
+      { idToken: token }.to_json,
+      { 'Content-Type' => 'application/json' }
+    ) do |response, _request, _result|
+      return response
+    end
+  end
+end
