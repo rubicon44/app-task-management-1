770d0c3e8be032c9466281da2755f46ed7647928
rubicon44 <rub.b.c44@gmail.com>
2024-11-06 23:14:21 +0900

    [Add]会員登録時のjwt検証機能を追加

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,5 +1,9 @@
 module V1
   class UsersController < ApiController
+    include ActionController::Cookies
+    skip_before_action :check_authenticate!, only: %i[create], raise: false
+    before_action :authenticate_user, only: [:create]
+
     def index
       users = User.all
       render json: users
@@ -11,12 +15,25 @@ module V1
     end
 
     def create
-      user = User.new(user_params)
+      if @firebase_id == user_params[:firebase_id]
+        user = User.new(user_params)
 
-      if user.save
-        render json: user, status: :created
+        if user.save
+          token = generate_token(@firebase_id)
+
+          user_attributes = {
+            id: user.id,
+            bio: user.bio,
+            nickname: user.nickname,
+            username: user.username,
+          }
+
+          render json: { user: user_attributes, token: token }, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
       else
-        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        render json: { error: 'Unauthorized user. UID does not match.' }, status: :unauthorized
       end
     end
 
@@ -36,6 +53,27 @@ module V1
     end
 
     private
+
+    def authenticate_user
+      token = request.headers['Authorization']
+      return render json: { error: 'Token not provided' }, status: :unauthorized if token.blank?
+
+      firebase_response = Firebase.get_user_by_token(token)
+      firebase_data = JSON.parse(firebase_response.body)
+
+      if firebase_data['users'] && firebase_data['users'].any?
+        @firebase_user = firebase_data['users'].first
+        @firebase_id = @firebase_user['localId']
+      else
+        render json: { error: 'Invalid token' }, status: :unauthorized
+      end
+    rescue RestClient::Unauthorized, RestClient::Forbidden
+      render json: { error: 'Token verification failed' }, status: :unauthorized
+    end
+
+    def generate_token(firebase_id)
+      JsonWebToken.encode({ firebase_id: firebase_id }, Settings.jwt.time_exp.hours.from_now, Settings.jwt.alg)
+    end
 
     def user_params
       params.require(:user).permit(:firebase_id, :bio, :email, :nickname, :username)
